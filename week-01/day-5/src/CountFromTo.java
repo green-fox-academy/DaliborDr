62c9824f46401a6077a909ebeef63ec3d85d7461
Dalibor <drozdadalibor@gmail.com>
2019-04-05 15:41:33 +0200

    I did 3 more tasks ending with DrawTriangle

--- week-01/day-5/src/CountFromTo.java
+++ week-01/day-5/src/CountFromTo.java
@@ -1,19 +1,27 @@
-
+import java.util.Scanner;
 public class CountFromTo {
     public static void main(String[] args){
-        
+        Scanner scanner = new Scanner(System.in);
+        int userInput1 = scanner.nextInt();
+        int userInput2 = scanner.nextInt();
+        if (userInput1 > userInput2){
+            System.out.println("The second number should be bigger");
+            }
+        else {
+            while (userInput1< userInput2) {
+                userInput1++;
+                System.out.println(userInput1);
+            }
+
+        }
+
+
+
 
 
 
 
     }
-
-
-
-
-
-
-
 
         // Create a program that asks for two numbers
 // If the second number is not bigger than the first one it should print:
@@ -34,6 +42,6 @@ public class CountFromTo {
 
 
 
-    }
+
 
 }
---
+++ week-01/day-5/src/DrawTriangle.java
@@ -0,0 +1,27 @@
+public class DrawTriangle {
+    public static void main(String[] args){
+       String Star = "*";
+       for (int lines = 0 ; lines< 4 ; lines++ ){
+           System.out.println(Star);
+           if (Star){
+               System.out.println(Star +Star);
+           }
+       }
+
+
+
+        // Write a program that reads a number from the standard input, then draws a
+// triangle like this:
+//
+// *
+// **
+// ***
+// ****
+//
+// The triangle should have as many lines as the number was
+
+
+
+
+    }
+}

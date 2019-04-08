38a4027a17b060f53442bffc06b03eaec2d988cb
Dalibor <drozdadalibor@gmail.com>
2019-04-08 14:09:26 +0200

    I have created 3 monday tasks ending with Appendfunction

--- week-02/day-1/src/Doubling.java
+++ week-02/day-1/src/Doubling.java
@@ -1,8 +1,18 @@
 public class Doubling {
-    public static void main(String[] args){
 
-
-
-        
+    public static int doubling(int baseNum) {
+        int y = baseNum * 2;
+        return y;
     }
+
+
+    public static void main(String[] args) {
+        // - Create an integer variable named `baseNum` and assign the value `123` to it
+// - Create a function called `doubling` that doubles it's input parameter and returns with an integer
+// - Print the result of `doubling(baseNum)`
+        System.out.println(doubling(123));
+
+
+    }
+
 }

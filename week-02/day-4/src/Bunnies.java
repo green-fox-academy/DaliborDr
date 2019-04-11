8c55bd35646397efa905afebd3927e26fd3cc902
Dalibor <drozdadalibor@gmail.com>
2019-04-11 16:55:47 +0200

    Another three tasks added, ended with Switchin Chars

--- week-02/day-4/src/Bunnies.java
+++ week-02/day-4/src/Bunnies.java
@@ -1,6 +1,11 @@
 public class Bunnies {
     public static void main(String[] args) {
-
+        System.out.println(ears(10));
     }
-    public static
+    public static int ears(int a){
+        if (a>0){
+            return ears(a-1)+2;
+        }
+        return a;
+    }
 }
---
+++ week-02/day-4/src/BunniesAgain.java
@@ -0,0 +1,13 @@
+public class BunniesAgain {
+    public static void main(String[] args) {
+    System.out.println(bunnies(5));
+    }
+    public static int bunnies(int a){
+        if (a>0 && a%2==0){
+            return bunnies(a-1)+3;
+        }
+        else if (a>0){
+            return bunnies(a-1)+2;
+        }return a;
+    }
+}

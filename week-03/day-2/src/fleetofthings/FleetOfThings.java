ca5b4de5a05bf2099c739a39a8d91074d53839b1
Dalibor <drozdadalibor@gmail.com>
2019-04-16 17:42:10 +0200

    Added 3 mire tasks, ende with ThingsOfFleet

---
+++ week-03/day-2/src/fleetofthings/FleetOfThings.java
@@ -0,0 +1,23 @@
+package fleetofthings;
+
+public class FleetOfThings {
+
+    public static void main(String[] args) {
+        Fleet fleet = new Fleet();
+        // Create a fleet of things to have this output:
+        // 1. [ ] Get milk
+        // 2. [ ] Remove the obstacles
+        // 3. [x] Stand up
+        // 4. [x] Eat lunch
+        Thing milk = new Thing("milk");
+        Thing removeTheObstackles = new Thing("Remove the obstacles");
+        Thing standUp = new Thing("Stand up");
+        Thing eatLunch = new Thing("Eat Lunch");
+        fleet.add(milk);
+        fleet.add(removeTheObstackles);
+
+
+
+        System.out.println(fleet);
+    }
+}

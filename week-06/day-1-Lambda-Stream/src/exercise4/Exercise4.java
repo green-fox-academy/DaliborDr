86e55b6824bf5cfe70912ac1e15c9db28499923b
Dalibor <drozdadalibor@gmail.com>
2019-05-14 15:08:53 +0200

    Created a 4th task with average value

--- week-06/day-1-Lambda-Stream/src/exercise4/Exercise4.java
+++ week-06/day-1-Lambda-Stream/src/exercise4/Exercise4.java
@@ -2,13 +2,25 @@ package exercise4;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.OptionalDouble;
 import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 public class Exercise4 {
     public static void main(String[] args) {
 //        Write a Stream Expression to get the average value of the odd numbers from the following list:
         List<Integer> numbers = Arrays.asList(1, 3, -2, -4, -7, -3, -8, 12, 19, 6, 9, 10, 14);
 
-        numbers.stream().filter(n -> n % 2 != 0).forEach();
+        double a =numbers.stream().filter(n -> n % 2 != 0).collect(Collectors.summingInt(n -> n));
+        System.out.println(a);
+        double average = a / numbers.stream().filter(n -> n % 2 !=0).collect(Collectors.toList()).size();
+        System.out.println(average);
+        //Better solution to solve this task...
+        OptionalDouble result = numbers.stream()
+                .filter(number -> !(number%2 ==0))
+                .mapToInt(number -> number)
+                .average();
+        System.out.println(result);
+
     }
 }

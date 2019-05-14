13f1e7ef5e4bee9471a6c561c234bdc5eae1a016
Dalibor <drozdadalibor@gmail.com>
2019-05-14 14:39:41 +0200

    Created 4 exercises with 3 tasks finished

---
+++ week-06/day-1-Lambda-Stream/src/exercise1/exercise1.java
@@ -0,0 +1,15 @@
+package exercise1;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class exercise1 {
+    public static void main(String[] args) {
+//        Write a Stream Expression to get the even numbers from the following list:
+        List<Integer> numbers = Arrays.asList(1, 3, -2, -4, -7, -3, -8, 12, 19, 6, 9, 10, 14);
+
+        List<Integer> newNumbers = numbers.stream().filter(n -> n %2 == 0).collect(Collectors.toList());
+        System.out.println(newNumbers);
+
+    }
+}

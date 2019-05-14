dd08f85ea0a87d7acb461a33f57250b6ae44b6a8
Dalibor <drozdadalibor@gmail.com>
2019-05-14 16:27:41 +0200

    Ihave created 2 more tasks, ended with frequency of characters in a string

--- week-06/day-1-Lambda-Stream/src/exercise8/Exercise8.java
+++ week-06/day-1-Lambda-Stream/src/exercise8/Exercise8.java
@@ -1,4 +1,18 @@
 package exercise8;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class Exercise8 {
+    public static void main(String[] args) {
+//        Write a Stream Expression to concatenate a Character list to a string!
+        List<Character> characters = new ArrayList<>();
+        characters.add('a');
+        characters.add('b');
+        characters.add('c');
+        String result = characters.stream().map(c -> c.toString()).collect(Collectors.joining());
+        System.out.println(result);
+
+    }
 }
---
+++ week-06/day-1-Lambda-Stream/src/exercise9/Exercise9.java
@@ -0,0 +1,9 @@
+package exercise9;
+
+public class Exercise9 {
+    public static void main(String[] args) {
+//        Write a Stream Expression to find the frequency of characters in a given string!
+        
+
+    }
+}
